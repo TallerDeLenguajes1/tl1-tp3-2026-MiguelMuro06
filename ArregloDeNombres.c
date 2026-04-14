6cb660a937309a615c4d09d259eff36af6059e91
rioma967 <muromiguel17@gmail.com>
2026-04-14 14:02:46 -0300

    mi segundo commit

--- ArregloDeNombres.c
+++ ArregloDeNombres.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#define Cantidad 50 
 int main(){
+
+    char *V[Cantidad];
+    char *buff;
+
+    buff = (char *) malloc ((strlen(V)+1)*sizeof(char));
+
     printf("Hola mundo");
     return 0;
 }
 
 /*
- void MostrarPersonas()
+ void MostrarPersonas( char* v[]){
+ 
+ }
 
 */
